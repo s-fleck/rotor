f2a135fe5294f4eb2be23d95aead76f1fa654733
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-03-31 07:49:33 +0200

    define rotate_size arguments

---
+++ R/rotate_size.R
@@ -0,0 +1,17 @@
+rotate_size <- function(
+  file,
+  size,
+  age,
+  max_backups,
+  compress,
+  postrotate,
+  postrotate_args,
+  prerotate,
+  prerotate_args,
+  dry_run,
+  verbose
+){
+
+}
+
+
