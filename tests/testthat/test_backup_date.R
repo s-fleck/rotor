4743e203f754a0895b5ffdfd4e164122ebf11f99
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-04-05 23:40:24 +0200

    test is_parsable_date

--- tests/testthat/test_backup_date.R
+++ tests/testthat/test_backup_date.R
@@ -95,3 +95,18 @@ test_that("parse_date works as expected", {
   expect_identical(parse_date("201802"), as.Date("2018-02-01"))
   expect_identical(parse_date("2018"), as.Date("2018-01-01"))
 })
+
+
+
+test_that("is_parsable_date works as expected", {
+  expect_true(is_parsable_date("2018-12-01"))
+  expect_true(is_parsable_date("20181201"))
+  expect_true(is_parsable_date("2018-02"))
+  expect_true(is_parsable_date("201802"))
+  expect_true(is_parsable_date("2018"))
+
+  expect_true(is_parsable_date(20181231))
+  expect_false(is_parsable_date(20181232))
+  expect_false(is_parsable_date("1 week"))
+  expect_false(is_parsable_date("2 years"))
+})
