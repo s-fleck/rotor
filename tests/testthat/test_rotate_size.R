2b9e3a134e7823319150b27da8172ae0d5e3b651
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-04-06 17:34:41 +0200

    renamed rotate/backup_interval to rotate/backup_time

--- tests/testthat/test_rotate_size.R
+++ tests/testthat/test_rotate_size.R
@@ -34,3 +34,28 @@ test_that("rotate_size works as expected", {
   file.remove(find_backups(tf))
   file.remove(tf)
 })
+
+
+
+
+
+# legacy tests ------------------------------------------------------------
+
+context("rotate_size")
+
+
+test_that("rotate_size works as expected", {
+  expect_identical(parse_size(123), 123L)
+  expect_identical(parse_size("2k"), 2048L)
+})
+
+
+
+test_that("parse_info_unit works", {
+  parse_info_unit("k")
+  parse_info_unit("m")
+  expect_error(parse_info_unit("r"))
+  expect_identical(parse_size("1k"), 1024L)
+})
+
+
