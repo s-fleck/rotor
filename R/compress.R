be248e943b229980edfbda1680908228e96a3dcc
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-04-09 00:48:27 +0200

    replaced compress_and_removed by copy_or_compress

--- R/compress.R
+++ R/compress.R
@@ -9,54 +9,63 @@
 #'
 #' @return `character` scalar: path to the compressed file
 #'
-compress_and_remove <- function(
+copy_or_compress <- function(
   file,
+  outname,
   compression  = TRUE,
   compression_level = 9,
-  remove = TRUE,
+  add_ext = TRUE,
+  overwrite = overwrite,
   dry_run = getOption("rotor.dry_run", FALSE),
   verbose = getOption("rotor.dry_run", dry_run)
 ){
   stopifnot(
     is_scalar_character(file),
+    is_scalar_character(outname),
     file.exists(file),
     is_scalar_character(compression) || is_scalar_logical(compression),
     is_scalar_integerish(compression_level),
-    is_scalar_logical(remove),
+    is_scalar_logical(add_ext),
     is_scalar_logical(dry_run),
     is_scalar_logical(verbose)
   )
 
-  if (isFALSE(compression)){
-    return(file)
-  }
-
-  if (isTRUE(compression)){
-    compression <- {
-      if (requireNamespace("zip", quietly = TRUE)) "zip" else "zip_base"
+  # init
+    if (isTRUE(compression)){
+      compression <-
+        {if (requireNamespace("zip", quietly = TRUE)) "zip" else "zip_base"}
     }
-  }
+
+    if (!isFALSE(compression))
+      outname <- paste0(outname, ".zip")
+
+    if (file.exists(outname)){
+      if (overwrite && !dry_run){
+        msg_file_remove(outname, dry_run, verbose)
+        file.remove(outname)
+      } else {
+        stop("Backup exists and `overwrite == FALSE`")
+      }
+    }
+
+    msg_file_copy(self$file, outname, dry_run, verbose)
+
+  # copy
+    if (isFALSE(compression) && !dry_run){
+      file.copy(file, outname, overwrite = overwrite)
+      return(outname)
+    }
 
   # zip
-  out <- paste0(file, ".zip")
-  assert(!file.exists(out))
-  if (verbose)   message("'", file, "' -> '", out, "'")
+    if (identical(compression, "zip")){
+      assert_namespace("zip")
+      if (!dry_run)  zip::zipr(outname, file, compression_level = compression_level)
 
-  if (identical(compression, "zip")){
-    assert_namespace("zip")
-    if (!dry_run)  zip::zipr(out, file, compression_level = compression_level)
+    } else if (identical(compression, "zip_base")){
+      owd <- setwd(dir = dirname(file))
+      on.exit(setwd(owd))
+      utils::zip(basename(outname), files = basename(file), flags="-q")
+    }
 
-  } else if (identical(compression, "zip_base")){
-    owd <- setwd(dir = dirname(file))
-    on.exit(setwd(owd))
-    utils::zip(basename(out), files = basename(file), flags="-q")
-  }
-
-  if (remove && !dry_run){
-    if (verbose) message("removing '", file, "'")
-    assert(file.exists(out))
-    unlink(file, recursive = TRUE)
-  }
-
-  out
+  outname
 }
