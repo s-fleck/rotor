aeaa4287435a0053192bdc4ee071b26b3504b8c3
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-04-28 19:07:38 +0200

    make parse_datetime tests less strict

--- tests/testthat/test_BackupQueueDateTime.R
+++ tests/testthat/test_BackupQueueDateTime.R
@@ -32,15 +32,15 @@ test_that("is_parsable_date works as expected", {
 
 test_that("parse_datetime works as expected", {
   d <- as.Date("2019-12-01")
-  expect_identical(parse_datetime(d), as.POSIXct(as.character(d)))
+  expect_equal(parse_datetime(d), as.POSIXct(as.character(d)))
 
-  expect_identical(parse_datetime("2018-12-01"), as.POSIXct("2018-12-01"))
-  expect_identical(parse_datetime("20181201"), as.POSIXct("2018-12-01"))
-  expect_identical(parse_datetime("2018-02"), as.POSIXct("2018-02-01"))
-  expect_identical(parse_datetime("201802"), as.POSIXct("2018-02-01"))
-  expect_identical(parse_datetime("2018"), as.POSIXct("2018-01-01"))
+  expect_equal(parse_datetime("2018-12-01"), as.POSIXct("2018-12-01"))
+  expect_equal(parse_datetime("20181201"), as.POSIXct("2018-12-01"))
+  expect_equal(parse_datetime("2018-02"), as.POSIXct("2018-02-01"))
+  expect_equal(parse_datetime("201802"), as.POSIXct("2018-02-01"))
+  expect_equal(parse_datetime("2018"), as.POSIXct("2018-01-01"))
 
-  expect_identical(
+  expect_equal(
     parse_datetime(c("2018-12-02", "20181201", "2018")),
     as.POSIXct(c("2018-12-02", "2018-12-01", "2018-01-01"))
   )
@@ -51,27 +51,27 @@ test_that("parse_datetime works as expected", {
 
   expect_identical(parse_datetime(d1), d1)
 
-  expect_identical(parse_datetime("2019-04-12--17-49-19"), d1)
-  expect_identical(parse_datetime("2019-04-12--17-49"), d2)
-  expect_identical(parse_datetime("2019-04-12----17"), d3)
+  expect_equal(parse_datetime("2019-04-12--17-49-19"), d1)
+  expect_equal(parse_datetime("2019-04-12--17-49"), d2)
+  expect_equal(parse_datetime("2019-04-12----17"), d3)
 
-  expect_identical(parse_datetime("2019-04-12T17-49-19"), d1)
-  expect_identical(parse_datetime("2019-04-12T17-49"), d2)
-  expect_identical(parse_datetime("2019-04-12T17"), d3)
+  expect_equal(parse_datetime("2019-04-12T17-49-19"), d1)
+  expect_equal(parse_datetime("2019-04-12T17-49"), d2)
+  expect_equal(parse_datetime("2019-04-12T17"), d3)
 
-  expect_identical(parse_datetime("2019-04-12T174919"), d1)
-  expect_identical(parse_datetime("2019-04-12T1749"), d2)
-  expect_identical(parse_datetime("2019-04-12T17"), d3)
+  expect_equal(parse_datetime("2019-04-12T174919"), d1)
+  expect_equal(parse_datetime("2019-04-12T1749"), d2)
+  expect_equal(parse_datetime("2019-04-12T17"), d3)
 
-  expect_identical(parse_datetime("20190412T174919"), d1)
-  expect_identical(parse_datetime("20190412T1749"), d2)
-  expect_identical(parse_datetime("20190412T17"), d3)
+  expect_equal(parse_datetime("20190412T174919"), d1)
+  expect_equal(parse_datetime("20190412T1749"), d2)
+  expect_equal(parse_datetime("20190412T17"), d3)
 
-  expect_identical(parse_datetime("20190412174919"), d1)
-  expect_identical(parse_datetime("201904121749"), d2)
-  expect_identical(parse_datetime("2019041217"), d3)
+  expect_equal(parse_datetime("20190412174919"), d1)
+  expect_equal(parse_datetime("201904121749"), d2)
+  expect_equal(parse_datetime("2019041217"), d3)
 
-  expect_identical(
+  expect_equal(
     parse_datetime(c("2019-04-12T17-49-19", "20190412T1749", "2019041217")),
     as.POSIXct(c(d1, d2, d3))
   )
