bebb428f8dfdeaabf96d09fe16cd1ee42302672a
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-05-05 11:42:22 +0200

    test internal/external zip command sepparately

--- tests/testthat/test_copy_or_compress.R
+++ tests/testthat/test_copy_or_compress.R
@@ -10,18 +10,41 @@ teardown({
 
 
 
-
-test_that("copy_or_compress works as expected", {
+test_that("copy_or_compress works with default zip command", {
   tf <- file.path(td, "compresstest.log")
+  on.exit(unlink(tf))
   saveRDS(iris, file = tf, compress = FALSE)
 
   r <- copy_or_compress(tf, tf, compression = TRUE)
+  on.exit(unlink(r), add = TRUE)
   expect_true(file.exists(r))
-  expect_identical(zip::zip_list(r)[1, ]$filename, "compresstest.log")
-  unlink(r)
+  identical(unzip(r, list = TRUE)[["Name"]], "compresstest.log")
+})
+
+
+
+
+test_that("copy_or_compress works with internal zip command", {
+  tf <- file.path(td, "compresstest.log")
+  on.exit(unlink(tf))
+  saveRDS(iris, file = tf, compress = FALSE)
 
   r <- copy_or_compress(tf, tf, compression = "base::zip")
+  on.exit(unlink(r), add = TRUE)
+  expect_true(file.exists(r))
+  identical(unzip(r, list = TRUE)[["Name"]], "compresstest.log")
+})
+
+
+
+
+test_that("copy_or_compress works with zip::zipr", {
+  tf <- file.path(td, "compresstest.log")
+  on.exit(unlink(tf))
+  saveRDS(iris, file = tf, compress = FALSE)
+
+  r <- copy_or_compress(tf, tf, compression = "zip::zipr")
+  on.exit(unlink(r), add = TRUE)
   expect_true(file.exists(r))
   expect_identical(zip::zip_list(r)[1, ]$filename, "compresstest.log")
-  expect_true(file.remove(tf))
 })
