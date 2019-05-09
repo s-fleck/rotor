24c47f1a3218b97aa039314d747820936b7bc5b9
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-05-09 08:15:24 +0200

    smarter test for is_zipcomd_available

--- tests/testthat/test_utils-predicates.R
+++ tests/testthat/test_utils-predicates.R
@@ -1,11 +1,15 @@
 context("utils-predicates")
 
 
-test_that("is_zipcmd_available works as expected", {
-  ori_zipcmd <- Sys.getenv("R_ZIPCMD")
-  on.exit(Sys.setenv(R_ZIPCMD = ori_zipcmd))
-
+test_that("is_zipcmd_available detects zipcommand", {
+  # can only return true on platforms with a zip command
+  skip_if_not(is_zipcmd_available(), "No zipcommand found")
   expect_true(is_zipcmd_available())
-  Sys.setenv(R_ZIPCMD = "aslkjghasdkjlghas")
-  expect_false(is_zipcmd_available())
+})
+
+
+
+
+test_that("is_zipcmd_available() detects missing zipcommand", {
+  expect_false(is_zipcmd_available("sdjkghsaghaskjghsagj"))
 })
