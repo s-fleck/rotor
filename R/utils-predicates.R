6648945919a9875cccb5383e6803cecdda2e4d41
Stefan Fleck <stefan.fleck@statistik.gv.at>
2019-05-09 15:59:51 +0200

    blank zipcmd means no zipcmd available

--- R/utils-predicates.R
+++ R/utils-predicates.R
@@ -154,10 +154,18 @@ assert_valid_compression <- function(compression){
 
 
 is_zipcmd_available <- function(cmd = Sys.getenv("R_ZIPCMD", "zip")){
-  if (is_windows()){
+
+  if (is_blank(cmd)){
+    return(FALSE)
+  }
+
+  if (.Platform$OS.type == "windows"){
     suppressWarnings(res <- system2("where", cmd, stderr = NULL, stdout = NULL))
   } else {
-    suppressWarnings(res <- system2("command", paste("-v", cmd), stderr = NULL, stdout = NULL))
+    res <- tryCatch(
+       system2("command", paste("-v", cmd), stderr = NULL, stdout = NULL),
+       warning = function(w) {warning(w); 99}
+    )
   }
 
   assert(is_scalar(res))
